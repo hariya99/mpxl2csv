896e843abfb288dfa1e38a72e4fdaf2158fed084
Harish Chauhan <harish.chauhan99@gmail.com>
2023-04-28 17:03:02 -0500

    Use ProcessPoolExecutor for multiprocessing. Add support for xls files use pathlib instead of os return generator object instead of list in _gel_xl_files method

--- mpxl2csv/mpxl2csv.py
+++ mpxl2csv/mpxl2csv.py
@@ -1,7 +1,11 @@
-import os, multiprocessing, warnings
-from typing import List 
+import multiprocessing
+from concurrent.futures import ProcessPoolExecutor
+import warnings
+from pathlib import Path
+from typing import Generator 
 import openpyxl as opx
-
+import xlrd
+# import time
 
 class Excel2Csv(object):
     """
@@ -14,13 +18,11 @@ class Excel2Csv(object):
             mp.convert(xl_path, csv_path) 
     """
 
-    def __init__(self, num_processes : int =2, 
-                 engine : str ="openpyxl",
+    def __init__(self, num_processes : int = 2, 
                  delimiter : str = "|") -> None:
         """
-            Utilize Python's multiprocessing module to convert .xlsx files to .csv files.
+            Utilize Python's multiprocessing module to convert .xlsx, xls files to .csv files.
             Param : num_processes: Number of processes to use for conversion
-            Param : engine: Engine to use for conversion. Currently only openpyxl is supported 
             Param : delimiter: Delimiter to use for .csv files
         """
         available_cores : str  = multiprocessing.cpu_count()
@@ -30,52 +32,90 @@ class Excel2Csv(object):
                         f"It is advisable to use a number of processes less " +
                         f"than or equal to the number of CPUs.")
         self.num_processes : int = num_processes
-        self.engine : str = engine
         self.delimiter : str = delimiter
+        self._extns : tuple = (".xlsx", ".xls")
 
 
     def convert(self, xl_base_path : str, csv_base_path : str) -> None:
         """
             Convert all .xlsx files in a directory to .csv files.
         """
-        xl_files : List[str] = self._get_xl_files(xl_base_path)
+        xl_files : Generator[Path, None, None] = self._get_xl_files(xl_base_path)
         self._convert_files(xl_files, csv_base_path)
 
-    def _get_xl_files(self, xl_base_path : str) -> List[str]:
+    def _get_xl_files(self, xl_base_path : str) -> Generator[Path, None, None]:
         """
-            Get all .xlsx files in a directory.
+            Get all .xlsx and .xls files from a directory.
             The directory can have subdirectories.
+            return a generator object
         """
-        xl_files = []
-        for root, dirs, files in os.walk(xl_base_path):
-            for file in files:
-                if file.endswith(".xlsx"):
-                    xl_files.append(os.path.join(root, file))
-        return xl_files 
+        return (path_obj for path_obj in 
+                Path(xl_base_path).rglob("*")
+                if path_obj.suffix in self._extns)
 
-    def _convert_files(self, xl_files : List[str], csv_base_path : str) -> None:
+    def _get_csv_files(self, csv_base_path : str) -> Generator[Path, None, None]:
         """
-            Convert all .xlsx files to .csv files.
+            Get all .csv files from a directory.
+            The directory can have subdirectories.
+            return a generator object
         """
-        processes = []
-        with multiprocessing.Pool(self.num_processes) as pool:
+        return (path_obj for path_obj in Path(csv_base_path).rglob("*.csv"))
+    
+    def _convert_files(self, xl_files : Generator[Path, None, None], 
+                       csv_base_path : str) -> None:
+        """
+            Convert all .xlsx and .xls files to .csv files.
+        """
+        # Method - 1 : Using multiprocessing module
+        # processes = []
+        # with multiprocessing.Pool(self.num_processes) as pool:
+        #     for xl_file in xl_files:
+        #         csv_file = Path(csv_base_path).joinpath(xl_file.stem + ".csv")
+        #         processes.append(pool.apply_async(self._convert_file, 
+        #                                           args=(str(xl_file), str(csv_file))))
+        #     for process in processes:
+        #         process.get()
+
+        # Method - 2 : Using concurrent.futures module
+        with ProcessPoolExecutor(max_workers=self.num_processes) as executor:
             for xl_file in xl_files:
-                csv_file = os.path.join(csv_base_path, os.path.basename(xl_file).replace(".xlsx", ".csv"))
-                processes.append(pool.apply_async(self._convert_file, args=(xl_file, csv_file)))
-            for process in processes:
-                process.get()
+                csv_file = Path(csv_base_path).joinpath(xl_file.stem + ".csv")
+                if xl_file.suffix == self._extns[0]:
+                    executor.submit(self._convert_file_opx, str(xl_file), str(csv_file))
+                else: 
+                    executor.submit(self._convert_file_xlrd, str(xl_file), str(csv_file))
 
     
-    def _convert_file(self, xl_path : str, csv_path : str )-> None:
+    def _convert_file_opx(self, xl_path : str, csv_path : str )-> None:
         """
             Convert a single .xlsx file to a .csv file using openpyxl 
         """
+        # print(f"Conversion started for file: {xl_path}")
+        # start_time = time.time()
         wb = opx.load_workbook(xl_path, read_only=True, data_only=True)
         ws = wb.active
-        with open(csv_path, "w") as f:
+        with open(csv_path, "w", encoding="utf-8") as f:
             for row in ws.rows:
-                row_values = [str(cell.value) for cell in row]
+                row_values = (str(cell.value) for cell in row)
                 f.write(self.delimiter.join(row_values) + "\n")
         
         wb.close()
+        # end_time = time.time()
+        # print(f"Time taken to convert to {csv_path} is {end_time - start_time} seconds")
 
+    def _convert_file_xlrd(self,xl_path : str, csv_path : str )-> None:
+        """
+            Convert a single .xls file to a .csv file using xlrd 
+        """
+        # print(f"Conversion started for file: {xl_path}")
+        # start_time = time.time()
+        wb = xlrd.open_workbook(xl_path)
+        ws = wb.sheet_by_index(0)
+        with open(csv_path, "w", encoding="utf-8") as f:
+            for row_num in range(ws.nrows):
+                row_values = (str(cell.value) for cell in ws.row(row_num))
+                f.write(self.delimiter.join(row_values) + "\n")
+        wb.release_resources()
+
+        # end_time = time.time()
+        # print(f"Time taken to convert to {csv_path} is {end_time - start_time} seconds")
--- tests/mp_tests.py
+++ tests/mp_tests.py
@@ -1,5 +1,7 @@
 import unittest
-import sys, os 
+from typing import Generator
+from pathlib import Path
+import os, sys 
 sys.path.append(os.path.abspath(os.path.join('..', 'mpxl2csv')))
 from mpxl2csv import Excel2Csv
 
@@ -8,6 +10,8 @@ class TestMP(unittest.TestCase):
 
     def setUp(self) -> None:
         self.mp = Excel2Csv()
+        self.input_path : str = str(Path(".").joinpath("resources", "sample_input"))
+        self.output_path : str = str(Path(".").joinpath("resources", "sample_output"))
     
     def tearDown(self) -> None:
         del self.mp 
@@ -15,11 +19,23 @@ class TestMP(unittest.TestCase):
     def test_warning(self):
         with self.assertWarns(UserWarning):
             mp = Excel2Csv(num_processes=9)
-        
+    
     def test_list_xl_files(self):
-        path = "../sample_data/"
-        xl_files = self.mp._get_xl_files(path)
-        self.assertEqual(len(xl_files), 3)
+        xl_files = self.mp._get_xl_files(self.input_path)
+        self.assertIsInstance(xl_files, Generator)
+
+    def test_input_file_type(self):
+        xl_files = self.mp._get_xl_files(self.input_path)
+        for xl_file in xl_files:
+            self.assertIsInstance(xl_file, Path)
+            self.assertRegex(xl_file.suffix, r"\.xlsx|\.xls")
+    
+    def test_ouput_file_type(self):
+        self.mp.convert(self.input_path, self.output_path)
+        csv_files = self.mp._get_csv_files(self.output_path)
+        for csv_file in csv_files:
+            self.assertIsInstance(csv_file, Path)
+            self.assertEqual(csv_file.suffix, ".csv")
 
 
 if __name__ == '__main__':
